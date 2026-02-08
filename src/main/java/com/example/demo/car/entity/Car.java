03d2ec475cbb1a8fec473dfa743426d15ab94d2a
Alexey <alex.nowik@mail.ru>
2026-02-08 23:19:46 +0400

    unit tests for CarService

--- src/main/java/com/example/demo/car/entity/Car.java
+++ src/main/java/com/example/demo/car/entity/Car.java
@@ -1,6 +1,7 @@
 package com.example.demo.car.entity;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,27 +9,33 @@ import java.time.OffsetDateTime;
 
 @Entity
 @Getter
-@Setter
 @Table(name = "car")
 public class Car {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter(AccessLevel.NONE)
     private Long id;
 
+    @Setter
     @Column(name = "reg_number", nullable = false, unique = true, length = 32)
     private String regNumber;
 
+    @Setter
     @Column(nullable = false, length = 100)
     private String model;
 
     @Column(name = "created_at", nullable = false, updatable = false)
+    @Setter(AccessLevel.NONE)
     private OffsetDateTime createdAt;
 
+    @Setter
     private Integer mileage;
 
+    @Setter
     @Column(name = "release_year")
     private Integer releaseYear;
 
+    @Setter
     @Column(length = 200)
     private String owner;
 
